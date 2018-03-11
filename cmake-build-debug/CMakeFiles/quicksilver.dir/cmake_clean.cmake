681e419b9d77084d44b5876046e948d6cb2f7b78
diqiudiqiu <kongxyee@gmail.com>
2018-03-11 18:27:24 +0100

    qs
    qs

---
+++ cmake-build-debug/CMakeFiles/quicksilver.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/quicksilver.dir/src/main.cpp.o"
+  "CMakeFiles/quicksilver.dir/src/RPQTree.cpp.o"
+  "CMakeFiles/quicksilver.dir/src/SimpleGraph.cpp.o"
+  "CMakeFiles/quicksilver.dir/src/SimpleEstimator.cpp.o"
+  "CMakeFiles/quicksilver.dir/src/SimpleEvaluator.cpp.o"
+  "quicksilver.pdb"
+  "quicksilver"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/quicksilver.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
