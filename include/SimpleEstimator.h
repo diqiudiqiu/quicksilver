b3ae94da481edf218b4cf319a575dda2d0ed627e
diqiudiqiu <kongxyee@gmail.com>
2018-04-04 12:14:24 +0200

    1
    1

--- include/SimpleEstimator.h
+++ include/SimpleEstimator.h
@@ -5,23 +5,46 @@
 #ifndef QS_SIMPLEESTIMATOR_H
 #define QS_SIMPLEESTIMATOR_H
 
+#include <map>
+#include <set>
+#include <string>
 #include "Estimator.h"
 #include "SimpleGraph.h"
-#include <map>
-static std::deque<uint32_t> convertTreeToDeque(RPQTree *q);
+
+
+std::vector<std::string> traverseTree(RPQTree *q);
+
+
+class CharacteristicSet {
+public:
+
+
+    std::vector<std::vector<uint32_t>> count;
+    std::vector<std::map<uint32_t,uint32_t>> frecount;
+    std::vector<std::map<uint32_t,uint32_t>> Sfrecount;
+
+protected:
+    uint32_t L;
+    uint32_t V;
+
+public:
+    void CreateCharacteristicSet(std::shared_ptr<SimpleGraph> &g);
+
+    void setNoVertices(uint32_t n);
+    void setNoLabels(uint32_t noLabels);
+
+
+    CharacteristicSet()= default;
+    ~CharacteristicSet() = default;
+
+
+};
+
 
 class SimpleEstimator : public Estimator {
 private:
     std::shared_ptr<SimpleGraph> graph;
-
-	std::map<uint32_t, uint32_t> T;
-    std::map<uint32_t, uint32_t> V;
-
-	uint32_t getNumberOfDistinctValuesForAttribute(uint32_t attribute);
-
-    uint32_t estimateSize(std::deque<uint32_t> relations);
-	uint32_t estimateSize(std::deque<uint32_t> relations, uint32_t Tr, uint32_t Vry);
-	uint32_t estimateNaturalJoinSize(uint32_t Ts, uint32_t Tr, uint32_t Vsy = 10, uint32_t Vry = 10);
+    CharacteristicSet CharacSet;
 
 public:
     explicit SimpleEstimator(std::shared_ptr<SimpleGraph> &g);
@@ -30,7 +53,14 @@ public:
     void prepare() override ;
     cardStat estimate(RPQTree *q) override ;
 
+    std::pair<uint32_t ,uint32_t > EstimateQueryCardinality(std::vector<std::string>, uint32_t L);
+    uint32_t estimateSize(std::vector<std::string> relations, uint32_t pos);
+    uint32_t estimateSize(std::vector<std::string> relations, uint32_t Tr, uint32_t Vry,uint32_t pos);
+    uint32_t estimateNaturalJoinSize(uint32_t Ts, uint32_t Tr, uint32_t Vsy = 10, uint32_t Vry = 10);
+    float CCC(uint32_t O, uint32_t S,uint32_t cl,uint32_t O1);
+
 };
+
 
 
 #endif //QS_SIMPLEESTIMATOR_H
--- src/SimpleEvaluator.cpp
+++ src/SimpleEvaluator.cpp
@@ -9,7 +9,7 @@ SimpleEvaluator::SimpleEvaluator(std::shared_ptr<SimpleGraph> &g) {
 
     // works only with SimpleGraph
     graph = g;
-    est = nullptr; // estimator not attached by default
+    est = nullptr;
 }
 
 void SimpleEvaluator::attachEstimator(std::shared_ptr<SimpleEstimator> &e) {
@@ -19,12 +19,13 @@ void SimpleEvaluator::attachEstimator(std::shared_ptr<SimpleEstimator> &e) {
 void SimpleEvaluator::prepare() {
 
     // if attached, prepare the estimator
-  //  if(est != nullptr) est->prepare();
+    // if(est != nullptr) est->prepare();
 
     // prepare other things here.., if necessary
 
 }
 
+/*
 cardStat SimpleEvaluator::computeStats(std::shared_ptr<SimpleGraph> &g) {
 
     cardStat stats {};
@@ -33,15 +34,31 @@ cardStat SimpleEvaluator::computeStats(std::shared_ptr<SimpleGraph> &g) {
         if(!g->adj[source].empty()) stats.noOut++;
     }
 
-    stats.noPaths = g->getNoDistinctEdges();
+   // stats.noPaths = g->getNoDistinctEdges();
 
     for(int target = 0; target < g->getNoVertices(); target++) {
         if(!g->reverse_adj[target].empty()) stats.noIn++;
     }
+    return stats;
+}*/
 
+cardStat SimpleEvaluator::computeStats1(std::map<uint32_t ,std::set<uint32_t >> table) {
+    cardStat stats {};
+    std::set<uint32_t > In;
+    stats.noOut=table.size();
+    for(auto list:table){
+
+        stats.noPaths+=list.second.size();
+        for(auto in:list.second){
+                In.insert(in);
+        }
+    }
+        //stats.noPaths+=a1.size()*a2.size();
+
+    stats.noIn=(uint32_t )In.size();
     return stats;
 }
-
+/*
 std::shared_ptr<SimpleGraph> SimpleEvaluator::project(uint32_t projectLabel, bool inverse, std::shared_ptr<SimpleGraph> &in) {
 
     auto out = std::make_shared<SimpleGraph>(in->getNoVertices());
@@ -139,8 +156,125 @@ std::shared_ptr<SimpleGraph> SimpleEvaluator::evaluate_aux(RPQTree *q) {
 
     return nullptr;
 }
+*/
+
+std::map<uint32_t ,std::set<uint32_t >> SimpleEvaluator::project1(uint32_t projectLabel, bool inverse ){
+    std::map<uint32_t ,std::set<uint32_t >> table;
+    if(!inverse) {
+        // going forward
+        for(uint32_t source = 0; source < graph->getNoVertices(); source++) {
+            std::set<uint32_t > From;
+            for (auto labelTarget : graph->adj[source]) {
+
+                auto label = labelTarget.first;
+                auto target = labelTarget.second;
+
+                if (label == projectLabel)
+                    From.insert(target);
+            }
+            if(!From.empty())
+            {
+                table.insert(std::make_pair(source,From));
+            }
+        }
+    } else {
+        // going backward
+
+        for(uint32_t source = 0; source < graph->getNoVertices(); source++) {
+            std::set<uint32_t > From;
+            for (auto labelTarget : graph->reverse_adj[source]) {
+
+                auto label = labelTarget.first;
+                auto target = labelTarget.second;
+
+                if (label == projectLabel)
+                    From.insert(target);
+            }
+            if(!From.empty())
+            {
+                table.insert(std::make_pair(source,From));
+            }
+        }
+    }
+    return table;
+}
+std::map<uint32_t ,std::set<uint32_t >> SimpleEvaluator::join1(std::map<uint32_t ,std::set<uint32_t >> &l ,std::map<uint32_t ,std::set<uint32_t >> &r){
+    std::vector<uint32_t > indexes;
+    for(auto left=l.begin();left!=l.end();left++)
+    {
+        //auto left=l[i];
+        std::set<uint32_t > target;
+        uint32_t flag=0;
+        for (auto source:left->second)
+        {
+            if(r.find(source)!=r.end())
+            {
+                flag=1;
+                target.insert(r.at(source).begin(),r.at(source).end());
+            }
+        }
+        if(flag)
+        {
+            l[left->first]=target;
+        } else{
+            indexes.push_back(left->first);
+//            l.erase(left->first);
+//            --left;
+        }
+    }
+    for(auto index:indexes)
+    {
+        l.erase(index);
+    }
+    return l;
+}
+
+std::map<uint32_t ,std::set<uint32_t >> SimpleEvaluator::evaluate_aux1(RPQTree *q){
+    if(q->isLeaf()) {
+        // project out the label in the AST
+        std::regex directLabel (R"((\d+)\+)");
+        std::regex inverseLabel (R"((\d+)\-)");
+
+        std::smatch matches;
+
+        uint32_t label;
+        bool inverse;
+
+        if(std::regex_search(q->data, matches, directLabel)) {
+            label = (uint32_t) std::stoul(matches[1]);
+            inverse = false;
+        } else if(std::regex_search(q->data, matches, inverseLabel)) {
+            label = (uint32_t) std::stoul(matches[1]);
+            inverse = true;
+        } else {
+            std::cerr << "Label parsing failed!" << std::endl;
+
+        }
+
+        return SimpleEvaluator::project1(label, inverse);
+    }
+
+    if(q->isConcat()) {
+
+        // evaluate the children
+        auto leftGraph = SimpleEvaluator::evaluate_aux1(q->left);
+        auto rightGraph = SimpleEvaluator::evaluate_aux1(q->right);
+
+        // join left with right
+        return SimpleEvaluator::join1(leftGraph, rightGraph);
+
+    }
+
+
+}
+
+
+
 
 cardStat SimpleEvaluator::evaluate(RPQTree *query) {
-    auto res = evaluate_aux(query);
-    return SimpleEvaluator::computeStats(res);
+    //auto res = evaluate_aux(query);
+    //table.resize(0);
+    auto res = evaluate_aux1(query);
+    return SimpleEvaluator::computeStats1(res);
+    //return SimpleEvaluator::computeStats(res);
 }
